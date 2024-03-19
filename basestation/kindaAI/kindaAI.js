db910917841db8a29958b81a16de004520f701df
wswoodruff <williamswoodruff@gmail.com>
2024-03-19 19:34:12 -0400

    Working on building object in 1 pass

--- basestation/kindaAI/kindaAI.js
+++ basestation/kindaAI/kindaAI.js
@@ -9,25 +9,27 @@ function isKindaPural(word) {
   return false;
 }
 
-function isNoun(word) {
-  if(word.noun) return true;
-  return false;
-}
-
-function isNumber(word) {
-  if(isNaN(word)) return false;
+// NOTE must be the string version of the word
+function isNumber(strWord) {
+  if(isNaN(strWord)) return false;
   return true;
 }
+
+// NOTE should be the dictionary version of the word
+const hasTag = (item, tag) => item?.tags.includes(tag);
+
+const ignoreWords = ["a", "the", "now"];
 
 // Research for later on taxonomy stuffs
 // If we have a polymorphic word we can use tags to differentiate how to use it
 var dictionary = {
-  "animals" : {
-    "cat": { tags: ["noun"] },
-    "dog": { tags: ["noun"] },
-    "whale": { tags: ["noun"] },
-    "fish": { tags: ["noun"] },
-    "giraffe": { tags: ["noun"] }
+  "nouns" : {
+    "cat": { tags: [] },
+    "dog": { tags: [] },
+    "whale": { tags: [] },
+    "fish": { tags: [] },
+    "giraffe": { tags: [] },
+    "tree": { tags: [] }
   },
 
   "verbs" : {
@@ -35,7 +37,7 @@ var dictionary = {
     "blast": { tags: ["action"] },
     "flutter": { tags: ["action"] },
     "rotate": { tags: ["action"] },
-    "spin": { tags: ["action"] }
+    "spin": { tags: ["action"] },
   },
 
   "adjectives" : {
@@ -43,38 +45,34 @@ var dictionary = {
     "small": { tags: ["modifier"] }
   },
 
-  "adverbs" : {
-    "around": { tags: ["action"] }
+  "prepositions": {
+    "above": {},
+    "below": {},
+    "beside": {},
+    "between": {},
+    "in": {},
+    "inside": {},
+    "into": {},
+    "near": {},
+    "on": {},
+    "over": {},
+    "through": {},
+    "under": {},
+    "underneath": {},
+    "against": {},
+    "alongside": {},
+    "around": {},
+    "behind": {},
+    "beyond": {},
+    "by": {},
+    "onto": {},
+    "off": {},
+    "out": {},
+    "up": {},
+    "down": {},
+    "across": {}
   }
 };
-
-const prepositions = [
-  "above",
-  "below",
-  "beside",
-  "between",
-  "in",
-  "inside",
-  "into",
-  "near",
-  "on",
-  "over",
-  "through",
-  "under",
-  "underneath",
-  "against",
-  "alongside",
-  "around",
-  "behind",
-  "beyond",
-  "by",
-  "onto",
-  "off",
-  "out",
-  "up",
-  "down",
-  "across"
-];
 
 // foreach and for each
 
@@ -92,31 +90,15 @@ const parsedDictionary = Object.entries(dictionary).reduce((newDict, [category, 
         ...collector,
         [itemName]: {
           ...itemVal,
-          name: itemName,
-          tags: [...itemVal.tags, category]
+          name: itemName, // Here for convenience
+          tags: [...itemVal.tags || [], category]
         }
       };
     }, {}))
   };
 }, {});
 
-const ignoreWords = ["a", "the", "now"];
-
-// console.log("parsedDictionary", parsedDictionary);
-
-const hasTag = (item, tag) => item.tags.includes(tag);
-
-/*
-  Ex: 3 dogs sleep under a tree
-  Working idea of what this could be
-  Example obj: {
-    subject: "dog",
-    number: 3,
-    action: "sleep",
-    preposition: "under",
-    preopositionObj: "tree"
-  }
-*/
+console.log("parsedDictionary", parsedDictionary);
 
 const parseStatementToActionObject = (statement) => {
 
@@ -124,42 +106,76 @@ const parseStatementToActionObject = (statement) => {
   const splitStatement = statement.split(" ")
     .filter((piece) => !ignoreWords.includes(piece));
 
-  const actionObject = splitStatement.reduce((collector, word) => {
+  // WE CURRENTLY ONLY SUPPORT 1 ADJECTIVE PER SUBJECT, AND 1 PER PREPOSITION OBJECT, 2 TOTAL
+  // Let's build on the rule for 1 adjective allowed each.
+  // We can figure how to pluralize later, sry I know that goes against the assume-an-array model,
+  // but let's try this we can version and update it later.
+
+  // Show final shape of the actionObject
+  const actionObject = {
+    subject: '', // First noun we encounter
+    number: '', // First num we encounter
+    adjective: '', // Arr of adjectives encountered before a preposition
+    preposition: '', // First preposition we encounter
+    prepositionObj: '', // 2nd noun we encounter
+    prepositionObjAdjective: '', // Rest of the adjectives we encounter
+    rest: [] // The remaining words
+  };
+
+  console.log('actionObject BEFORE', actionObject);
+
+  splitStatement.forEach((word) => {
 
     let formattedWord = word.toLowerCase();
 
+    // Check if it's a number first
+    if (!isNaN(formattedWord)) {
+
+      actionObject.number = Number(formattedWord);
+      return;
+    }
+
+    // Normalize the word so we can better look it up in the parsedDictionary
     if (isKindaPural(word)) {
       formattedWord = formattedWord.slice(0, -1);
     }
 
     console.log('formattedWord', formattedWord);
-    const dictMatch = parsedDictionary[formattedWord];
 
-    if (!dictMatch) {
+    const wordObj = parsedDictionary[formattedWord];
+
+    if (!wordObj) {
       console.warn('WARNING NO DICTIONARY MATCH for :', formattedWord);
-      return collector;
+      return;
     }
 
-    const newInfo = {};
+    const isNoun = hasTag(wordObj, "nouns");
+    const isAdjective = hasTag(wordObj, "adjective");
 
-    if (!collector[dictMatch.name] && hasTag(dictMatch, 'noun')) {
-      newInfo.subject1 = { ...dictMatch };
+    const isPreposition = hasTag(wordObj, "prepositions");
+
+    if (!actionObject.subject && isNoun) {
+      actionObject.subject = wordObj;
     }
-
-    return {
-      ...collector,
-      ...newInfo
-    };
+    else if (isNoun) {
+      actionObject.prepositionObj = wordObj;
+    }
+    else if (isAdjective) {
+      actionObject.adjectives.push(wordObj);
+    }
+    else if (isPreposition) {
+      actionObject.preposition = wordObj;
+    }
   }, {});
 
   console.log("splitStatement", splitStatement);
-  console.log("actionObject", actionObject);
+  console.log('actionObject AFTER', actionObject);
 };
 
 // parseStatement("10 cats fly around a dog");
-// parseStatementToActionObject("3 dogs sleep under a tree");
+parseStatementToActionObject("3 dogs flutter under a tree");
 
 // We're looking to build an object to parse the sentence into.
 //
 
-export {parseStatementToActionObject, hasTag, ignoreWords, parsedDictionary, prepositions, isKindaPural, isNoun, isNumber, dictionary}
+// export {parseStatementToActionObject, hasTag, ignoreWords, parsedDictionary, prepositions, isKindaPural, isNoun, isNumber, dictionary}
