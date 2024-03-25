b9810df42790f637125c8501ee8923f75054cc0e
tripdragon <nickitdesign@gmail.com>
2024-03-25 16:07:32 -0400

    base 1 text parcer

---
+++ makin a new parser.js
@@ -0,0 +1,113 @@
+
+
+
+
+
+game()
+^ bind game is starting
+maintains a in context catch all object
+w = {}
+which gets unique object references
+["a","b"].join(" ")
+w["a b"] = instantance thingy
+
+
+load taco car model "dkfjngkdjfgjfg"
+(load __ __ model url)
+
+var aa = 'load taco car model "dkfjngkdjfgjfg"';
+var bb = aa.split(" ");
+// (load __ __ model url)
+
+var a1 = bb.indexOf("load")
+var a2 = bb.indexOf("model")
+var a3 = bb[bb.length-1];
+cc = bb.slice(a1+1,a2)
+
+// if index 0 and 1 have an inbetween, and a string at length-1
+// or unknown words form a var then load+model have a string at length-1
+// run
+var t = loadModel(url){}
+t.onload = ?¿
+
+if(a1 is 0)
+
+// cc here could BE the var
+// cc === gg["taco", "car"] false!! this is good
+// ["taco", "car"] = function(x){console.log(x)} fail, darn it
+// "a" = function(){} also fail
+// but 
+w = {}
+w["fishsticks"] = 4
+will work!!
+hmmmm
+as will w["narf fish"] = 4
+
+["a","b"].join(" ")
+
+so second taco car
+etc, since each is unique on {}
+
+
+if(cc !== ""){
+  
+}
+
+alternative vs splitting
+and also look to regex
+a1 = aa.indexOf("load")
+a2 = aa.indexOf("model")
+
+
+on load cars position is 10 29 48
+scale is 2
+turn y 2.4959
+run some magic other set of functions
+
+b = func
+func b(x,b,m)
+log m
+let gg = b * 727634 / gofunc(m)
+if gg == (==== is the older truthy)
+if gg >= 9
+then we should flip cat with force 2
+return mm
+}
+
+
+
+taco car position = 10 20 2 rotation = pi/2 0 0
+( nodes have stored "taco car" id )
+( position being a prop, = being a statement like otherwise parse next fex numbers by spaces or commas )
+( rotation is in euler, since spaces rule, then keep math to no spaces for here )
+
+its driving forward at speed 2
+(its references state of current subject)
+(driving being a feature yet to be determined, but is an effector to be a heading vector)
+(speed is attribute of it)
+
+let m = 4
+let m 4
+let m -> 4
+m = 4
+( = is preferred, its one character and says what it is )
+( let is preferred cause its math notation and short, its also easy to parse the page for variables )
+
+set stage
+setStage()
+( ¿¿¿¿ )
+
+load building 1 model "dfkjngdkfjg"
+on load 
+
+
+lets sort things events like
+"on load"
+
+
+var aa = "on load";
+var bb = aa.split(" ")
+var hold = x => {console.log(x)}
+if(bb[0]+bb[1] === "onload"){
+  hold = (url) => { console.log(`im loading ${url} !`) }
+}
